2588512d8dcb9f148ac79177204672074791df56
BOBBY <yunchen.li@my.jcu.edu.au>
2025-05-30 13:41:33 +0800

    Add a check for correct passwords and give relative feedback

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,3 +2,16 @@ import getpass
 
 true_password = "151515"
 
+def main():
+    password = get_password()
+    if password == true_password:
+        print("You are login Successful")
+    else:
+        print("You are login Failure")
+
+def get_password():
+    password = getpass.getpass("Enter your password: ")
+    return password
+
+if __name__ == "__main__":
+    main()
