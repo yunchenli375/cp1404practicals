f04773be04dfd835002e5dbc7193fcc245a6508b
BOBBY <yunchen.li@my.jcu.edu.au>
2025-05-30 13:44:37 +0800

    Add customized password

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,17 +1,3 @@
 import getpass
 
 true_password = "151515"
-
-def main():
-    password = get_password()
-    if password == true_password:
-        print("You are login Successful")
-    else:
-        print("You are login Failure")
-
-def get_password():
-    password = getpass.getpass("Enter your password: ")
-    return password
-
-if __name__ == "__main__":
-    main()
