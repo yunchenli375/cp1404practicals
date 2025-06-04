ceef8dc99dec78725a3c6aecc199adb1169929b0
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-04 18:36:14 +0800

    Add open file and print

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,3 +3,9 @@ out_file = open("name.txt", "w")
 name = input("What is your name? ")
 print(name, file=out_file, end="")
 out_file.close()
+
+# question 2
+in_file = open("name.txt", "r")
+name = in_file.read()
+print(f"Hi {name}!")
+in_file.close()
