0dd123a14f918047d14de94e2706703631977ff8
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-04 18:37:06 +0800

    Added reading the first two numbers and printing the result

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,3 +9,12 @@ in_file = open("name.txt", "r")
 name = in_file.read()
 print(f"Hi {name}!")
 in_file.close()
+
+# question 3
+with open("numbers.txt") as in_file:
+    sum = 0
+    # to keep the integrity of DRY rules is not as intuitive as it seems
+    for line, _ in zip(in_file, range(2)):
+        sum += int(line)
+    print(sum)
+
