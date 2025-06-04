4bba4b1c944556cdcbbdbfa81ccc357cb033a5a0
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-04 13:20:34 +0800

    Add run program and show total number of days simulated

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -7,6 +7,7 @@ a 50% chance that it decreases by 0 to 5%.
 If the price rises above $1000, or falls below $0.01, the program should end.
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
 """
+
 import random
 
 MAX_INCREASE = 0.1  # 10%
@@ -15,8 +16,9 @@ MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
+day = 0
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+print(f"Starting price: ${price:,.2f}")
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -32,4 +34,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    day += 1
+    print(f"On day {day} price is: ${price:,.2f}")
