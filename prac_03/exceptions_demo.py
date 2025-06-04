bbfab97e8d598b01ba96c0b49960024aab4b666d
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-04 14:53:38 +0800

    Add answers to questions 1, 2, and 3

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,8 +2,13 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+When failed to convert the input to an integer.
+
 2. When will a ZeroDivisionError occur?
+When the denominator input is number 0.
+
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+Add a check to ensure the denominator is not zero before performing the division.
 """
 
 try:
