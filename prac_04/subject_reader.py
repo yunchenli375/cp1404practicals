f239158fb0a53e88b75e052c8c2af48a9be1a893
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-12 17:44:09 +0800

    Add nested list form

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,26 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    display_subject_details(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
+        parts = line.split(",")  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        parts[2] = int(
+            parts[2]
+        )  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
-
+    return data
 
 main()
