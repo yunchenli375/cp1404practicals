2481da985d4c07f3be1c26dfd508d827c22a84c3
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-12 17:33:21 +0800

    Add the answer to question 2

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -30,3 +30,6 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # Change the first element of numbers to "ten" (the string, not the number 10)
 numbers[0] = "ten"
 
+# Change the last element of numbers to 1
+numbers[-1] = 1
+
