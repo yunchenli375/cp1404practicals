b08cd0feb3d1433254a0585748844c9dde23b6d4
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-12 17:56:48 +0800

    Adds randomly generated numbers in the specified range

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,3 +9,23 @@ LINE_LENGTH = 6
 MIN = 1
 MAX = 45
 
+
+def main():
+    picks = int(input("How many quick picks? "))
+    for i in range(picks):
+        quick_pick = unique_sampling()
+        quick_pick.sort()
+        print(" ".join(f"{number:2}" for number in quick_pick))
+
+
+def unique_sampling():
+    """return a list containing LINE_LENGTH unique random numbers between MIN and MAX."""
+    result = []
+    while len(result) < LINE_LENGTH:
+        number = random.randint(MIN, MAX)
+        if number not in result:
+            result.append(number)
+    return result
+
+
+main()
