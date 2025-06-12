2f1281d88db45688913fde9c00218d7720913a78
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-12 17:49:23 +0800

    Adds a list of users who check their username for authorization

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -15,4 +15,27 @@ def main():
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
 
+    usernames = [
+        "jimbo",
+        "giltson98",
+        "derekf",
+        "WhatSup",
+        "NicolEye",
+        "swei45",
+        "BaseInterpreterInterface",
+        "BaseStdIn",
+        "Command",
+        "ExecState",
+        "InteractiveConsole",
+        "InterpreterInterface",
+        "StartServer",
+        "bob",
+    ]
+    username = input("Enter your username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
+
 main()
