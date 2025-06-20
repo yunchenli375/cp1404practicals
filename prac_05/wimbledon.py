e28ba8cf1756382f7ec49cbd44bf2da6e1e3fc6f
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-20 13:44:34 +0800

    Add initial character

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,11 @@
+"""
+Wimbledon
+Estimate: 15 minutes
+Actual:   11 minutes
+"""
+
+
+def main():
+    """program entrypoint"""
+    names_to_championships = {}
+    countries_with_champions = set()
