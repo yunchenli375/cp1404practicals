041e25e9b777ce607c523154454e7b501d575b19
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-20 13:21:21 +0800

    Adds the selected color

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Practical
+Hexadecimal colour codes in a dictionary
+"""
+
+HEX_COLOURS = {
+    "black": "#000000",
+    "gray": "#BEBEBE",
+    "white": "#FFFFFF",
+    "red1": "#FF0000",
+    "green1": "#00FF00",
+    "blue1": "#0000FF",
+    "yellow1": "#FFFF00",
+    "cyan1": "#00FFFF",
+    "magenta1": "#FF00FF",
+}
