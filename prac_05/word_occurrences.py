701d753e2b3c69ca4128bb0a8e3adf472b7cd8f7
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-20 13:29:50 +0800

    Add command to check for blank symbols

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,3 +8,8 @@ Actual:   5 minutes
 def main():
     """program entrypoint"""
     text = input("Enter some text: ")
+    word_count = {}
+    longest_word_length = 0
+    for word in text.split():
+        word = word.lower().strip(".,!?\"'()[]{};:")
+
