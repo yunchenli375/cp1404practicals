9b8b3ad1ad84b4c997723e475f665c1b7b179b45
BOBBY <yunchen.li@my.jcu.edu.au>
2025-06-20 13:44:45 +0800

    Add read and data processin

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,3 +9,15 @@ def main():
     """program entrypoint"""
     names_to_championships = {}
     countries_with_champions = set()
+    # utf-8 encoding must be specified, otherwise it will fail with a UnicodeDecodeError
+    with open("wimbledon.csv", encoding="utf-8") as in_file:
+        # skip the header line
+        in_file.readline()
+        for entry in in_file:
+            # strip is used to removing the trailing newline character
+            items = entry.strip().split(",")
+            countries_with_champions.add(items[1])
+            names_to_championships[items[2]] = (
+                names_to_championships.get(items[2], 0) + 1
+            )
+
