0e754021a57a9e12e8151aedf21bdcdb4d7e3284
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-01 17:02:18 +0800

    Define __str__, get_age, is_vintage

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -23,3 +23,15 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """returns a readable string of the instance"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """returns the age of the guitar"""
+        return datetime.date.today().year - self.year
+
+    def is_vintage(self):
+        """returns whether the guitar is 50 or more years old"""
+        return self.get_age() >= self.VINTAGE_THRESHOLD_YEARS
