fa5a247f1369dbbacf04d4b241ce21169434ef64
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-01 17:35:10 +0800

    Constructors (__init__ methods)

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -8,3 +8,16 @@ Actual time: 13 minutes
 class ProgrammingLanguage:
     """Represent a programming language object."""
 
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a ProgrammingLanguage instance.
+
+        name: str, the name of the programming language
+        typing: str, could be either "Dynamic" or "Static"
+        reflection: bool, whether the language supports reflection
+        year: int, the year when the language was invented
+        """
+        self.name = name
+        self.dynamic = typing == "Dynamic"
+        self.reflection = reflection
+        self.year = year
+
