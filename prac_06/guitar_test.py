4856e5d59b4448b2fb87fca04d125d446ef560c9
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-01 17:18:38 +0800

    Imports and constants

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,13 @@
+"""
+CP1404 Practical
+testing of guitar.py
+"""
+
+from guitar import Guitar
+import datetime
+
+GIBSON_L5_CES_YEAR = 1922
+GIBSON_L5_CES_COST = 16035.4
+ANOTHER_GUITAR_YEAR = 2013
+
+
