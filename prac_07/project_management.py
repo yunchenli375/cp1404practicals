c29db562f0e8252c2e594774acf464ddffc7f3b7
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-10 17:37:49 +0800

    Add imports and constants

---
+++ prac_07/project_management.py
@@ -0,0 +1,17 @@
+"""
+CP1404 Practical
+Load and save a data file and use a list of Project objects
+"""
+
+from project import ProjectManager
+
+DEFAULT_DATA_FILE = "projects.txt"
+
+MAIN_MENU = """- (L)oad projects
+- (S)ave projects
+- (D)isplay projects
+- (F)ilter projects by date
+- (A)dd a new project
+- (U)pdate a project"""
+
+
