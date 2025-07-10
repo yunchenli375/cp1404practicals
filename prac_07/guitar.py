66ea60c7afde5657965ab066813abf1c1df98bac
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-10 11:22:48 +0800

    Constructors

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -10,4 +10,15 @@ import datetime
 class Guitar:
     """Represent a Guitar object."""
 
+    VINTAGE_THRESHOLD = 50
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar object.
+
+        name: str, the name of the guitar
+        year: int, the year of the guitar
+        cost: float, the cost of the guitar"""
+        self.name = name
+        self.year = year
+        self.cost = cost
 
