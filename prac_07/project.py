bc8be0a3b473abba9277a17615b0071a591b36d5
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-10 17:34:28 +0800

    Add imports and constants

---
+++ prac_07/project.py
@@ -0,0 +1,15 @@
+"""
+CP1404 Practical
+Estimated time: 45 minutes
+Actual time: 120 minutes
+"""
+
+from datetime import datetime
+import csv
+
+DATE_FORMAT = "%d/%m/%Y"
+FILE_DELIMITER = "\t"
+
+
+
+
