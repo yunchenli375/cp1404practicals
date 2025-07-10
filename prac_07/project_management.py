83f62a5b32c1f679dc359f4f0c4f55df84238d0a
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-10 17:38:02 +0800

    Add menu options

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -15,3 +15,35 @@ MAIN_MENU = """- (L)oad projects
 - (U)pdate a project"""
 
 
+def main():
+    """program entrypoint"""
+    print("Welcome to Pythonic Project Management")
+    path = DEFAULT_DATA_FILE
+    manager = initialize_project_manager(path)
+    choice = menu(MAIN_MENU, "LSDFAUQ")
+    while choice != "Q":
+        if choice == "L":
+            new_path = input("Enter the filename to load projects from: ")
+            try:
+                manager = initialize_project_manager(new_path)
+                path = new_path
+            except FileNotFoundError:
+                print(f"File {new_path} not found. No update made.")
+        elif choice == "S":
+            new_path = input("Enter the filename to save projects to: ")
+            if len(new_path) == 0:
+                print("No filename provided, not saving.")
+            else:
+                manager.dump(open(new_path, "w"))
+        elif choice == "D":
+            manager.display()
+        elif choice == "F":
+            manager.display_filtered_projects_by_date()
+        elif choice == "A":
+            manager.add_project_interactively()
+        else:  # U
+            manager.update_project_interactively()
+        choice = menu(MAIN_MENU, "LSDFAUQ")
+    prompt_to_save(manager, path)
+    print("Thank you for using custom-built project management software.")
+
