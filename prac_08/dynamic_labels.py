68359ff104686293156544609711a7535732887f
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-19 09:33:06 +0800

    Define the main application class, inherited from App

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,12 @@
+"""
+CP1404 Practical
+Kivy GUI program having dynamic labels
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    """Kivy App to demonstrate dynamic labels."""
