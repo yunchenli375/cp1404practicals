58b5dfcd42efa811eb0777f891d16f83187aeeb6
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-19 09:29:06 +0800

    Defining constants

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,12 @@
+"""
+CP1404 Practical
+Kivy GUI program to convert miles to kilometers
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+KM_PER_MILE = 1.60934
+
+
