1f4eb7771a3adcb33795a19ab0cc52ef22187ed8
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-19 09:29:17 +0800

    Defining the main application class, inherited from App

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -10,3 +10,8 @@ from kivy.properties import StringProperty
 KM_PER_MILE = 1.60934
 
 
+class MilesToKmConverterApp(App):
+    """Kivy App to convert miles to kilometers."""
+
+    output_text = StringProperty()
+
