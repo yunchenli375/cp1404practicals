41eadeff113b2b123ee06b5447dcd53bc5e22651
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-19 09:25:17 +0800

    Applying initialization methods

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -11,3 +11,13 @@ from kivy.core.window import Window
 
 __author__ = "Lindsay Ward and Yunchen Li"
 
+
+class SquareNumberApp(App):
+    """SquareNumberApp is a Kivy App for squaring a number"""
+
+    def build(self):
+        """build the Kivy app from the kv file"""
+        Window.size = (200, 100)
+        self.title = "Square Number"
+        self.root = Builder.load_file("squaring.kv")
+        return self.root
