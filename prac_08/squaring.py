e21a4f4fbdf2afa602b3011c4f3ce178263dac16
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-19 09:24:44 +0800

    Author information

---
+++ prac_08/squaring.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 Practical
+Kivy GUI program to square a number
+Lindsay Ward, IT@JCU, modified by Yunchen Li
+Started 13/10/2015, modified in 2025
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+__author__ = "Lindsay Ward and Yunchen Li"
+
