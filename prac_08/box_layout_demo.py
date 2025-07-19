9a6d78179b5c3e045487c75f6b2de0e42ed4a872
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-19 09:20:25 +0800

    Handle the “Greetings” button click event

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -7,3 +7,8 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file("box_layout.kv")
         return self.root
+
+    def handle_greet(self):
+        print("greet")
+        print("test")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
