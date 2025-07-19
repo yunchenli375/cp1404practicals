f109327b4c44203d3d90c68a4ea81f15ea1b74ee
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-19 09:20:14 +0800

    Define the main application class

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file("box_layout.kv")
+        return self.root
