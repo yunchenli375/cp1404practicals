f24936d792455204d48d307cc0389a6b65bc6042
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:10:41 +0800

    Assertion test fare

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -11,4 +11,8 @@ def main():
     """Test SilverServiceTaxi class."""
     my_taxi = SilverServiceTaxi("Hummer", 200, 2)
     my_taxi.drive(18)
+    # asserting float equality with a tolerance
+    assert abs(my_taxi.get_fare() - 48.80) < float_info.epsilon
 
+
+main()
