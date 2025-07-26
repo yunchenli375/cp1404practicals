7cf899d2ef4b4c5f19b2354c8e94fd0a2be9c9cd
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:10:25 +0800

    Main function definition

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -7,12 +7,3 @@ from silver_service_taxi import SilverServiceTaxi
 from sys import float_info
 
 
-def main():
-    """Test SilverServiceTaxi class."""
-    my_taxi = SilverServiceTaxi("Hummer", 200, 2)
-    my_taxi.drive(18)
-    # asserting float equality with a tolerance
-    assert abs(my_taxi.get_fare() - 48.80) < float_info.epsilon
-
-
-main()
