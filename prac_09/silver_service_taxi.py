bbc6b63c0bce215267b49f01afc379234c99f66c
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:07:43 +0800

    Class definitions and documentation strings

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,3 +5,14 @@ SilverServiceTaxi class
 
 from taxi import Taxi
 
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that offers silver service."""
+
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.price_per_km *= fanciness
+
