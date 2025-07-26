03719bb4a36e134e3187f1d9e6442c472a72a6a1
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:01:55 +0800

    Import statements

---
+++ prac_09/band.py
@@ -0,0 +1,13 @@
+"""
+CP1404 Practical
+Band class
+It seems that all members of a band could only play guitar,
+which is not consistent with our cognition of a band.
+It is better to use a more general class(abstract base class) like Instrument,
+and let the Guitar class inherit from it.
+Then the musician's instruments should be a list of Instrument objects.
+"""
+
+from musician import Musician
+
+
