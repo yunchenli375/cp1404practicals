8e7c66b95aa9c2c2da8ff4484b51712a2afa1530
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:13:15 +0800

    Constructors

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -10,3 +10,9 @@ class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
     price_per_km = 1.23
+
+    def __init__(self, name, fuel):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.current_fare_distance = 0
+
