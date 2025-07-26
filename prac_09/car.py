0add4144824f8eb84f2ddd6230e9a4bd049a52e3
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:05:03 +0800

    Add mileage method

--- prac_09/car.py
+++ prac_09/car.py
@@ -24,3 +24,17 @@ class Car:
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
         self.fuel += amount
+
+    def drive(self, distance):
+        """Drive the car a given distance.
+
+        Drive given distance if car has enough fuel
+        or drive until fuel runs out return the distance actually driven.
+        """
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += distance
+        return distance
