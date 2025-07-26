f319dde5e95374f31f96f787783f09eb3c8176ed
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:19:44 +0800

    Constructor

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -10,3 +10,12 @@ class TaxiSimulator:
     MENU = "q)uit, c)hoose taxi, d)rive"
     CHOICE = "QCD"
 
+    def __init__(self, taxis):
+        """Initialise the simulator with no taxis."""
+        self.taxis = taxis
+        # instead of using a reference to the taxi object,
+        # which looks like a copy in Python and may cause confusion,
+        # we maintain an index of the current taxi
+        self.current_idx = -1
+        self.fare = 0.0
+
