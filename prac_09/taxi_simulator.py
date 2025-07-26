01fc597a3285eda8c16e9475db50a4ca49275b8a
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:19:56 +0800

    Drive method

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -30,3 +30,24 @@ class TaxiSimulator:
             choice = input(">>> ")
         return choice.upper()[0]
 
+    def drive(self):
+        """attempt to drive the current taxi"""
+        if self.current_idx == -1:
+            print("You need to choose a taxi before you can drive")
+        else:
+            distance = input("Drive how far? ")
+            while not distance.isdigit() or int(distance) < 0:
+                print("Invalid distance")
+                distance = input("Drive how far? ")
+            distance = int(distance)
+            # if the start_fare method returns the taxi object itself,
+            # we can chain the method calls!
+            self.taxis[self.current_idx].start_fare()
+            self.taxis[self.current_idx].drive(distance)
+            trip_fare = self.taxis[self.current_idx].get_fare()
+            print(
+                f"Your {self.taxis[self.current_idx].name} trip cost you ${trip_fare:.2f}"
+            )
+            self.fare += trip_fare
+        print(f"Bill to date: ${self.fare:.2f}")
+
