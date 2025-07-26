1cf2d797b60be4c0707039c70eeea152bbbc08b5
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:21:23 +0800

    Main function definition

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -6,3 +6,14 @@ Taxi class test code
 from taxi import Taxi
 
 
+def main():
+    """docstring"""
+    my_taxi = Taxi("Prius 1", 100)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
