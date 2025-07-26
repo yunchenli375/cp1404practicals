995d19ecb5e1e50bdd0bd57fdc29b2e3f8a0b86b
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:13:29 +0800

    Method for obtaining fare

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -19,3 +19,7 @@ class Taxi(Car):
     def __str__(self):
         """Return a string like a Car but with current fare distance."""
         return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return round(self.price_per_km * self.current_fare_distance, 1)
