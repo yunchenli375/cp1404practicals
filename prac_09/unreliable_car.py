222f4a0ed388916162c433851ec96dd003fb0fb3
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:22:28 +0800

    Constructors

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -10,3 +10,9 @@ from car import Car
 
 class UnreliableCar(Car):
     """Specialised version of a Car that is unreliable."""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
