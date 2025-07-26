3e1e2ccb823e893528d8aa28044a9bcf40505c22
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:04:30 +0800

    Constructor

--- prac_09/car.py
+++ prac_09/car.py
@@ -7,3 +7,13 @@ Car class
 class Car:
     """Represent a Car object."""
 
+    def __init__(self, name="", fuel=0):
+        """Initialise a Car instance.
+
+        name: string, reference name for car
+        fuel: float, one unit of fuel drives one kilometre
+        """
+        self.name = name
+        self.fuel = fuel
+        self.odometer = 0
+
