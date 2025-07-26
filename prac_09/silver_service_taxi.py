9bacd16e74c772a14d22038be00c0c901f191f44
BOBBY <yunchen.li@my.jcu.edu.au>
2025-07-26 14:07:53 +0800

    Class variables

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -16,3 +16,7 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.price_per_km *= fanciness
 
+    def __str__(self):
+        """Return a string representation of a SilverServiceTaxi."""
+        return f"{super().__str__()}, plus flagfall of ${self.flagfall:.2f}"
+
