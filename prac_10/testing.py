632046d795ee64e4461d4226cf770000ef283cbc
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-01 16:39:04 +0800

    is_long_word function

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -44,3 +44,16 @@ def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
     return " ".join(s for _ in range(n))
 
+
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
+
