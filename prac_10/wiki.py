d49b6d108f02393f4de5024b5a43bda93ade1cb5
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-01 16:47:38 +0800

    main function

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -7,4 +7,23 @@ details of the page
 import wikipedia
 
 
+def main():
+    """program entrypoint"""
+    query = input("Enter page title: ")
+    while query != "":
+        try:
+            page = wikipedia.page(query, auto_suggest=False)
+            print(page.content.splitlines()[0])
+            print(page.url)
+        except wikipedia.exceptions.DisambiguationError as e:
+            print(
+                "We need a more specific title. Try one of the following, or a new search:"
+            )
+            print(e.options)
+        except wikipedia.exceptions.PageError:
+            print("Page id does not match any pages. Try another id!")
+        print()
+        query = input("Enter page title: ")
+    print("Thank you.")
+
 
