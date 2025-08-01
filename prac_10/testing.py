2fdb3d1117c74374b00a43fe096871f1b48c986e
BOBBY <yunchen.li@my.jcu.edu.au>
2025-08-01 16:38:42 +0800

    Car class definition

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -5,4 +5,38 @@ Testing code using assert and doctest
 
 import doctest
 
+# prac_06/car.py is the modified version that contains a name field
+# from prac_06.car import Car
+
+
+# The original Car class without a name field is manually copied here
+class Car:
+    """Represent a Car object."""
+
+    def __init__(self, fuel=0):
+        """Initialise a Car instance.
+
+        fuel: float, one unit of fuel drives one kilometre
+        """
+        self.fuel = fuel
+        self._odometer = 0
+
+    def add_fuel(self, amount):
+        """Add amount to the car's fuel."""
+        self.fuel += amount
+
+    def drive(self, distance):
+        """Drive the car a given distance.
+
+        Drive given distance if car has enough fuel
+        or drive until fuel runs out return the distance actually driven.
+        """
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self._odometer += distance
+        return distance
+
 
